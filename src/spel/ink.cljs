6855c0d20770f9d1014351ffb83ba9a9a4b26315
Arne Brasseur <arne@arnebrasseur.net>
2021-04-24 18:11:12 +0200

    comment out ink.js

--- src/spel/ink.cljs
+++ src/spel/ink.cljs
@@ -2,24 +2,25 @@
   (:require ["inkjs/dist/ink.js" :refer [Story]])
   (:require-macros [spel.ink :refer [inline-file]]))
 
-(def json (subs (inline-file "resources/public/scenario.json") 1))
+(comment
+  (def json (subs (inline-file "resources/public/scenario.json") 1))
 
-(.toString (fn [] (js/JSON.parse json)))
+  (.toString (fn [] (js/JSON.parse json)))
 
-(subs json 1)
+  (subs json 1)
 
-(def story (Story. json))
+  (def story (Story. json))
 
-(.Continue story)
-(.-canContinue story)
-(.-currentChoices story)
-(.ChooseChoiceIndex story 0)
+  (.Continue story)
+  (.-canContinue story)
+  (.-currentChoices story)
+  (.ChooseChoiceIndex story 0)
 
-(.ChoosePathString story "gang")
+  (.ChoosePathString story "gang")
 
-(js-keys (.-variablesState story))
+  (js-keys (.-variablesState story))
 
-(.BindExternalFunction story "foo" (fn [] (js/console.log "in foo")))
+  (.BindExternalFunction story "foo" (fn [] (js/console.log "in foo"))))
 
 ;;  EXTERNAL playSound(soundName)
 ;;  ~ playSound("thing")
